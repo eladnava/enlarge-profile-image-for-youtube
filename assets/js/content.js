613e455f591d584ea7798405c7e45742f7a1a639
Elad Nava <eladnava@gmail.com>
2018-08-01 11:08:01 +0800

    Clean up comments & reformat code

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,102 +1,46 @@
-//----------------------------
-// Define search and replace
-// terms for image src link
-//----------------------------
-
+// Define search and replace terms for image src link
 var search = "s100";
 var replace = "s512";
 
-//----------------------------
-// Get profile image containers
-//----------------------------
-
-function getProfileImageContainer()
-{
-    //----------------------------
-    // Query it by class
-    //----------------------------
-
+function getProfileImageContainer() {
+    // Get profile image containers
     return $(".channel-header-profile-image-container");
 }
 
-//----------------------------
-// Get profile image tag
-//----------------------------
-
-function getProfileImage( container )
-{
-    //----------------------------
-    // Query it by tag name
-    //----------------------------
-
+function getProfileImage(container) {
+    // Get profile image tag
     return container.find("img");
 }
 
-//----------------------------
-// Inject link to larger image
-//----------------------------
-
-function injectEnlargeLink()
-{
-    //----------------------------
+function injectEnlargeLink() {
     // Get profile image link
-    //----------------------------
-
     var imageContainer = getProfileImageContainer();
 
-    //----------------------------
     // No image?
-    //----------------------------
-
-    if ( ! imageContainer.length )
-    {
+    if (!imageContainer.length) {
         return;
     }
 
-    //----------------------------
     // Get img tag
-    //----------------------------
-
     var imageTag = getProfileImage(imageContainer);
 
-    //----------------------------
     // No tag?
-    //----------------------------
-
-    if ( ! imageTag.length )
-    {
+    if (!imageTag.length) {
         return;
     }
 
-    //----------------------------
     // Get image src
-    //----------------------------
-
     var src = imageTag.attr('src');
 
-    //----------------------------
-    // Replace image pixel
-    // value in URL
-    //----------------------------
-
+    // Replace image pixel value in URL
     src = src.replace(search, replace);
 
-    //----------------------------
-    // Set link to point to
-    // the larger image
-    //----------------------------
-
+    // Set link to point to the larger image
     imageContainer.attr('href', src);
 
-    //----------------------------
     // Open in a new tab
-    //----------------------------
-
     imageContainer.attr('target', '_blank');
 }
 
-//----------------------------
 // One-time injection
-//----------------------------
-
 injectEnlargeLink();
